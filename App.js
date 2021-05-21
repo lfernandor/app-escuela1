51f1cfe8e1f72407ef415977c021a6ae588c39e4
iserm-git <58581611+iserm-git@users.noreply.github.com>
2021-05-21 18:39:32 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Homes from "./app/screens/Home";
+import Cuentas from "./app/screens/Cuenta";
+import Articulos from "./app/screens/Articulo";
+import Tiendas from "./app/screens/Tienda";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Tiendas />
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/screens/Articulo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+export default function Articulo() {
+    return (
+        <View>
+            <Text>CATALOGO</Text>
+        </View>
+    );
+}
